57f46d1644019da39191b12a9b26bcb9a7f4e1e8
Boston343 <branden.austin1@gmail.com>
2022-09-04 14:04:42 -0400

    now plays sound based on button press, or on keyboard keypress

--- js/index.js
+++ js/index.js
@@ -1,53 +1,59 @@
 
-var audio_tom1  = new Audio("sounds/tom-1.mp3");
-var audio_tom2  = new Audio("sounds/tom-2.mp3");
-var audio_tom3  = new Audio("sounds/tom-3.mp3");
-var audio_tom4  = new Audio("sounds/tom-4.mp3");
-var audio_crash = new Audio("sounds/crash.mp3");
-var audio_kick  = new Audio("sounds/kick-bass.mp3");
-var audio_snare = new Audio("sounds/snare.mp3");
-
+// ----------------------------------------------------------------------------------------------------------
 // add event listener for each item with class ".drum" using "forEach" and "addEventListener"
 document.querySelectorAll(".drum").forEach(item => {
     item.addEventListener("click", function () {
-        var letter = this.innerHTML;
-        switch (letter) {
-            case "w":
-                audio_tom1.play();
-                break;
-
-            case "a":
-                audio_tom2.play();
-                break;
-
-            case "s":
-                audio_tom3.play();
-                break;
-
-            case "d":
-                audio_tom4.play();
-                break;
-
-            case "j":
-                audio_crash.play();
-                break;
-
-            case "k":
-                audio_kick.play();
-                break;
-
-            case "l":
-                audio_snare.play();
-                break;
-
-            default:
-                break;
-        }
-
+        var key = this.innerHTML;
+        playSound(key);
     })
 })
 
-//  use "keydown"
+// ----------------------------------------------------------------------------------------------------------
+// add event listener for keypress
+document.addEventListener("keydown", function(event) {
+    playSound(event.key);
+});
 
-// var audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
+// ----------------------------------------------------------------------------------------------------------
+// Title
+function playSound(key) {
+    switch (key) {
+        case "w":
+            var audio_tom1  = new Audio("sounds/tom-1.mp3");
+            audio_tom1.play();
+            break;
+
+        case "a":
+            var audio_tom2  = new Audio("sounds/tom-2.mp3");
+            audio_tom2.play();
+            break;
+
+        case "s":
+            var audio_tom3  = new Audio("sounds/tom-3.mp3");
+            audio_tom3.play();
+            break;
+
+        case "d":
+            var audio_tom4  = new Audio("sounds/tom-4.mp3");
+            audio_tom4.play();
+            break;
+
+        case "j":
+            var audio_crash = new Audio("sounds/crash.mp3");
+            audio_crash.play();
+            break;
+
+        case "k":
+            var audio_kick  = new Audio("sounds/kick-bass.mp3");
+            audio_kick.play();
+            break;
+
+        case "l":
+            var audio_snare = new Audio("sounds/snare.mp3");
+            audio_snare.play();
+            break;
+
+        default:
+            break;
+    }
+}
